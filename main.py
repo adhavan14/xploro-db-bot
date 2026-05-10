a1ce7c8caf4be00be6bdaf59ec7aa94c5b868ca7
Adhavan.G <adhavanganesan14@gmail.com>
2026-05-10 17:17:12 +0530

    Allow cors

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://xploro-db-bot.vercel.app"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
